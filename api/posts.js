f107ca1a73eed7cd05787a206c71d4b619541433
ThankGod George <gthankgod@gmail.com>
2019-11-16 19:47:02 -0800

    Added backend features for user post

--- api/posts.js
+++ api/posts.js
@@ -1,10 +1,90 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 
+const Post = require('../models/Post');
+const User = require('../models/User');
+const Profile = require('../models/Profile');
 
-// @route GET api/users
+// @route POST api/post
 // @desc  Test route
-// @access Public
-router.get('/', (req, res) => res.send('Post route'));
+// @access Private
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+
+            const post = await newPost.save();
+            res.json(post);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    });
+
+
+// @route GET api/post
+// @desc  Get all posts
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        return res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route GET api/post/:id
+// @desc  Get a single post
+// @access Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) return res.status(404).json({ msg: 'Post not found ' });
+        return res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') return res.status(404).json({ msg: 'Post not found ' });
+        res.status(500).send('Server error');
+    }
+});
+
+// @route DELETE api/post/:id
+// @desc  DELETE a post
+// @access Private
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+        await post.remove();
+        res.json({ msg: 'Post removed' });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') return res.status(404).json({ msg: 'Post not found ' });
+        res.status(500).send('Server error');
+    }
+});
 module.exports = router;
