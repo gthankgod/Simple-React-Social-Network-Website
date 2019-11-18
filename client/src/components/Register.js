1f63d1518e3996ba34420138470f104f22738477
ThankGod George <gthankgod@gmail.com>
2019-11-18 14:03:05 -0800

    Added reducers and actions for redux store

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 import { setAlert } from '../actions/alert';
+import { register } from '../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -19,7 +20,7 @@ const Register = ({ setAlert }) => {
         if (password !== password2) {
             return setAlert('Passwords do not match', 'danger');
         }
-        console.log('success');
+        register({ name, email, password });
     }
     return <Fragment>
         <h1 className="large text-primary">Sign Up</h1>
@@ -64,7 +65,8 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
